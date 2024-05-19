b60c3e5e6ba0c03df5e4f0a7e8c13cdb43a6c061
Areasus <cantosh.s@gmail.com>
2024-05-19 13:21:51 +0545

    initially adding files

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+numbers = np.zeros(10)  ##creating an array of 10 0's
+print(numbers)
+matrix = np.zeros((3,4))  #creating matrix of 3x4(RXC) containing 0's
+print(matrix)
+n=[1,2,3,4,5]
+n1=np.array(n)
+print(type(n1))  #converting list to np array type
